0c7d6e7331e599b71f435704856408ee82603599
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-09-23 08:32:40 +0200

    Refactored classes into functional components and made small changes to the graphical representation

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Greeting extends React.Component<{ name: string }> {
-  render() {
-    return <h1 className="greeting">Hello {this.props.name}!</h1>;
-  }
+function Greeting(props: any) {
+  return <h1 className="greeting">Hello {props.name}!</h1>;
 }
 
-class InputField extends React.Component<{}, { nameValue: string, showingGreeting: boolean }> {
-  constructor(props: any) {
-    super(props);
-    this.state = { nameValue: '', showingGreeting: false };
-  }
+function InputField() {
+  let [nameValue, updateState] = useState<string>('');
+  let [showingGreetings, toggleGreetings] = useState<boolean>(false);
 
-  updateState(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ nameValue: event.target.value });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2 className="nameText">Insert your name below</h2>
-        <input className="inputField" onChange={(e) => this.updateState(e)}></input>
-        <button className="submitButton" onClick={() => this.setState({showingGreeting : true})}>Submit</button>
-        {this.state.showingGreeting && <Greeting name={this.state.nameValue} />}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2 className="nameText">Insert your name below</h2>
+      <input className="inputField" autoFocus={true} onChange={(e) => updateState(nameValue = e.target.value)}></input>
+      <span className="span"></span>
+      <button className="submitButton" onClick={() => toggleGreetings(showingGreetings = true)}>Submit</button>
+      <span className="span"></span>
+      {showingGreetings && <Greeting name={nameValue} />}
+    </div>
+  );
 }
 
-class Logo extends React.Component {
-  logoSVG = <img className="logo" src={logo} alt={"React logo"} width={500}></img>;
+function Logo() {
+  let logoSVG = <img className="logo" src={logo} alt={"React logo"} width={500}></img>;
 
-  render() {
-    return (
-      <div>{this.logoSVG}</div>
-    );
-  }
+  return (
+    <div>{logoSVG}</div>
+  );
 }
 
 function App() {
