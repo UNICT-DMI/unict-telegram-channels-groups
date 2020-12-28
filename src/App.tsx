12eb26da8da3b7bf5d144c1e6a9258891c5b7b69
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-12-28 11:38:18 +0100

    Started using Sass instead of css. Updated dependencies and gitignore file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Menu } from './Menu/Menu';
-import { Channels } from './Channels/Channels';
-import { Groups } from './Groups/Groups';
-import { Bots } from './Bots/Bots';
-import './App.css';
+import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {Menu} from './Menu/Menu';
+import {Channels} from './Channels/Channels';
+import {Groups} from './Groups/Groups';
+import {Bots} from './Bots/Bots';
+import './App.scss';
 
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <HashRouter basename="/">
-        <Menu />
-        <Switch>
-          <Route exact path="/channels">
-            <Channels />
-          </Route>
-          <Route exact path="/groups">
-            <Groups />
-          </Route>
-          <Route exact path="/bots">
-            <Bots />
-          </Route>
-          <Redirect to="/channels" />
-        </Switch>
-      </HashRouter>
-      <p className="credits">
-        Coded by
-        <a href="https://github.com/Lorenzo-Pappalardo">Lorenzo Pappalardo</a>
-        <a href="https://github.com/Helias">Stefano Borzì</a>
-        <a href="https://github.com/Gigi-G">Luigi Seminara</a>
-      </p>
-    </div>
-  );
+export default function App(): JSX.Element {
+   return (
+      <div className="App">
+         <HashRouter basename="/">
+            <Menu/>
+            <Switch>
+               <Route exact path="/channels">
+                  <Channels/>
+               </Route>
+               <Route exact path="/groups">
+                  <Groups/>
+               </Route>
+               <Route exact path="/bots">
+                  <Bots/>
+               </Route>
+               <Redirect to="/channels"/>
+            </Switch>
+         </HashRouter>
+         <p className="credits">
+            Coded by
+            <a href="https://github.com/Lorenzo-Pappalardo">Lorenzo Pappalardo</a>
+            <a href="https://github.com/Helias">Stefano Borzì</a>
+            <a href="https://github.com/Gigi-G">Luigi Seminara</a>
+         </p>
+      </div>
+   );
 }
-
-export default App;
