e4beea72ebf3d686c475b5877b845b00327f700d
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-03-24 15:29:32 +0200

    Added master's degree groups names

---
+++ src/Groups/MasterGroups.ts
@@ -0,0 +1,36 @@
+interface Subject {
+  title: string;
+  code: string;
+}
+
+export const firstYearGroupsNames: Subject[] = [
+  { title: 'ALGORITMI E COMPLESSITA', code: 'd73lytc' },
+  { title: 'CRITTOGRAFIA', code: 'hc9v2gu' },
+  { title: 'FONDAMENTI DI ANALISI DATI', code: 'wjbote3' },
+  { title: 'INGEGNERIA DEI SISTEMI DISTRIBUITI', code: 'g0le51h' },
+  { title: 'MULTIMEDIA', code: '28vtwgp' },
+  { title: 'OTTIMIZZAZIONE', code: 'f6olju2' },
+  { title: 'ANALISI NUMERICA', code: 'a02uce2' },
+  { title: 'BIG DATA', code: 'zlpjgic' },
+  { title: 'BLOCKCHAIN E CRYPTOCURRENCIES', code: 'c2ipibm' },
+  { title: 'COMPUTABILITA', code: 'kyprfw2' },
+  { title: 'COMPUTAZIONE NATURALE', code: '0w88ij6' },
+  { title: 'COMPUTER SECURITY', code: 'x43kd4z' },
+  { title: 'COMPUTER VISION', code: 'sf90883' },
+  { title: 'FONDAMENTI E LINGUAGGI PER LA PROGRAMMAZIONE DISTRIBUITA', code: '8ea329g' },
+  { title: 'MACHINE LEARNING', code: 'j8rls3j' },
+  { title: 'PROGRAMMAZIONE DI SISTEMI ROBOTICI AUTONOMI', code: 'ipwouav' },
+];
+
+export const secondYearGroupsNames: Subject[] = [
+  { title: 'ADVANCED PROGRAMMING LANGUAGES', code: 'uq0embk' },
+  { title: 'BIOINFORMATICA', code: '35pfgd1' },
+  { title: 'CRYPTOGRAPHIC ENGINEERING', code: '69va22r' },
+  { title: 'INTELLIGENZA ARTIFICIALE', code: '9i6ixq5' },
+  { title: 'LINGUAGGI FORMALI', code: '28xafrz' },
+  { title: 'PEER TO PEER AND WIRELESS NETWORKS', code: 'cdt2hgq' },
+  { title: 'SISTEMI CLOUD, IOT', code: 'ru8x5mv' },
+  { title: 'WEB REASONING', code: 'zso2z7h' },
+  { title: 'INGLESE', code: '' },
+  { title: 'TECNOLOGIE PER I SERVIZI WEB', code: '' },
+];
